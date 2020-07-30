3eac02f4317b9067eb37379bfb9801b75b69b13c
NickBulbeck <bulbecknick@gmail.com>
2020-07-30 18:31:43 +0100

    Interim commit to change Macs...

--- js/mrProsser.js
+++ js/mrProsser.js
@@ -3,19 +3,33 @@
 const randomAPI =          'https://randomapi.com/api/2d3d134dea88edb7fa46222808ec8ff0'; // correct
 const randomAPIwithError = 'https://randomapi.com/api/2d3d134dea88edb7fa46222808ec8ff'; // missing final 0
 const dontPanic = document.getElementsByTagName('H1')[0];
+const theAnswer = 42;
 
 const zaphod = () => {
   let benjyMouse = [];
-  for (let i=1; i<= 42; i++) {
+  for (let i=1; i<= theAnswer; i++) {
     let trillian = {name:{}};
-    trillian.name.first = "We apologise for";
-    trillian.name.last = "the inconvenience";
+    trillian.firstname = "We apologise for";
+    trillian.lastname = "the inconvenience";
     trillian.email = "wafti@dolmansaxlil.com";
     benjyMouse.push(trillian);
   }
   return benjyMouse;
 }
 
+const trinTragula = (prefect,dent) => {
+  // prefect is the randomuser array lf 42 folk;
+  // dent is the array of stuff fae randomapi which is in the following format:
+  // name: "First Last"
+  // email: "first.last@dolmansaxlil.frogstar"
+  for (let i=0; i<theAnswer; i++) {
+    console.log(`prefect[i]: ${prefect[i]}; dent[i]: ${dent[i]}`);
+    // prefect[i].name.first = dent[i].firstname;
+    // prefect[i].name.last = dent[i].lastname;
+    // prefect[i].email = dent[i].email;
+  }
+  return prefect;
+} 
 
 const milliways = (ford) => {
 /*
@@ -29,8 +43,41 @@ const milliways = (ford) => {
 
 */
   console.log("In milliways (should be an array immediately next)...");
-  console.log(ford.results); // this is an array of employee objects
-  
+  ford = ford.results;
+  console.log(ford);
+  let arthur;
+  let beeblebrox;
+  const marvin = new Promise(function(resolve,reject) {
+    // 1: create xml request
+    const xml = new XMLHttpRequest();
+    // 2: create callback function for when the stuff gets back
+    xml.onreadystatechange = () => {
+      if (xml.readyState === 4) {
+        if (xml.status === 200) {
+          arthur = JSON.parse(xml.responseText).results;
+          console.log(arthur);
+          beeblebrox = trinTragula(ford,arthur);
+          resolve(beeblebrox);
+          dontPanic.textContent = "Mostly harmless"
+        } else {
+          beeblebrox = trinTragula(ford,zaphod());
+          resolve(beeblebrox);
+          dontPanic.textContent = "Mostly harmless"
+        }
+      }
+    }
+    // 3: open the request
+    xml.open('GET',randomAPI);
+    // 4: send the request
+    xml.send();
+
+    const start = new Date().getTime();
+    let end = start;
+    while(end < start + 2000) {
+      end = new Date().getTime();
+    }
+  });
+  return marvin;
 }
 
 const bewareOfTheLeopard = () =>{
@@ -50,10 +97,9 @@ const bigYellowBulldozer = (event) => {
   }
   usersDisplayed = 42;
   dontPanic.textContent = "Harmless";
-  console.log("before either .then");
   data_getEmployees(usersDisplayed)
     .then( output => milliways(output))
-    .then( console.log("in the second .then"));
+    .then( output => dontPanic.textContent = output);
   // this is getting an array of 42 employees; so far, so good. So, next, I need to
   // pass this Promise to milliways() 
   // https://randomuser.me/api/?results=${usersDisplayed}&exc=login&noinfo
