e019d91792b9c209e33d8d55b21e38e56619575d
NickBulbeck <bulbecknick@gmail.com>
2020-08-02 09:45:01 +0100

    Added comments for dataAccess.js

--- js/dataAccess.js
+++ js/dataAccess.js
@@ -1,4 +1,16 @@
+/*
+  dataAccess.js handles the main retrieval of data from the interweb. As and when the app is further developed
+  to include database lookups, and/or other api calls, these will be handled here.
+  URL information is included in this file, so that scripts.js can work independently of where the data
+  is stored.
+*/
 
+/**************************************************************************************
+* data_getEmployees: gets a list of employee objects from randomuser.me api; list-length
+*                    set by the input parameter.
+* @param: listLength, integer
+* @return: Promise comprising an array of employee objects, or an Error object
+***************************************************************************************/
 
 const data_getEmployees = (listLength) => {
   const url = `https://randomuser.me/api/?results=${listLength}&exc=login&noinfo`; // correct
